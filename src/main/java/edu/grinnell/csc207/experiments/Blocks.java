70862301cf5473edf020989941ef227d9711d486
Ryanalys <ryanalys@grinnell.edu>
2024-11-17 20:58:09 -0600

    Fixed style errors

--- src/main/java/edu/grinnell/csc207/experiments/Blocks.java
+++ src/main/java/edu/grinnell/csc207/experiments/Blocks.java
@@ -16,8 +16,8 @@ import java.io.PrintWriter;
  * Experiments with ASCII blocks.
  *
  * @author Samuel A. Rebelsky
- * @author Your Name Here
- * @author Your Name Here
+ * @author Alyssa Ryan
+ * @author Slok Rajbhandari
  */
 public class Blocks {
   /**
